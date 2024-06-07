16380cc351ae2735f521382ee1216892cfccbdca
Jean Y <fyacar_14@hotmail.com>
2024-06-07 10:54:01 -0500

    Pregunta01 - Segundo Commit - Pregunta terminada

---
+++ src/main/java/pe/edu/cibertec/DAAII_T1_YayaCarbajalJeanFranco/model/dto/ResultadoDto.java
@@ -0,0 +1,9 @@
+package pe.edu.cibertec.DAAII_T1_YayaCarbajalJeanFranco.model.dto;
+
+import lombok.Builder;
+
+@Builder
+public class ResultadoDto {
+    private Boolean respuesta;
+    private String mensaje;
+}
