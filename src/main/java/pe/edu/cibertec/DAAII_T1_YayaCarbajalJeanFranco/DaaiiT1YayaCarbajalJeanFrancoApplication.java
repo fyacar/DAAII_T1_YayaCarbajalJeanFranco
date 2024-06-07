d1370f5445942b031ff3e68b3342441faff59ba3
Jean Y <fyacar_14@hotmail.com>
2024-06-07 10:34:41 -0500

    Pregunta01 - Primer commit - Iniciando Proyecto

---
+++ src/main/java/pe/edu/cibertec/DAAII_T1_YayaCarbajalJeanFranco/DaaiiT1YayaCarbajalJeanFrancoApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAAII_T1_YayaCarbajalJeanFranco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaaiiT1YayaCarbajalJeanFrancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaaiiT1YayaCarbajalJeanFrancoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DAAII_T1_YayaCarbajalJeanFranco/DaaiiT1YayaCarbajalJeanFrancoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAAII_T1_YayaCarbajalJeanFranco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaaiiT1YayaCarbajalJeanFrancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
